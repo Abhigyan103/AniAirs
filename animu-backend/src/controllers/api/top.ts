c3d017b7de4949cfb88e58fd4c445e55eb7880ca
Abhigyan Prakash Singh <abhigyansingh56@gmail.com>
2024-01-08 11:03:09 +0530

    Added documentation example in top controller

--- animu-backend/src/controllers/api/top.ts
+++ animu-backend/src/controllers/api/top.ts
@@ -4,10 +4,18 @@ import axios from "axios";
 
 const api = 'https://api.jikan.moe/v4/top';
 
+
+/**
+ * Retrieves the top rated anime currently.
+ * @async
+ * @param {express.Request} req - Express request object
+ * @param {express.Request} res - Express response object
+ * @throws {CustomAPIError} When API is not reponding or the request is not valid
+ */
 export const getTopAnime=async (req:express.Request, res:express.Response) =>{
     const data =(await axios.get(api+'/anime')).data;
 
-    res.json(data);
+    return res.json(data);
 }
 
 //TODO: getTopReviews
