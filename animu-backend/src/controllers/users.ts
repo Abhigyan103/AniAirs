6ed649f26089493079439e16de076c5f579c9e25
Abhigyan Prakash Singh <abhigyansingh56@gmail.com>
2024-01-08 11:02:44 +0530

    Added type User

--- animu-backend/src/controllers/users.ts
+++ animu-backend/src/controllers/users.ts
@@ -1,5 +1,18 @@
 import express from 'express';
 import { deleteUserById, getUserById, getUsers } from '../db/users';
+import mongoose from 'mongoose';
+
+type User = {
+    readonly _id: mongoose.ObjectId,
+    username: string,
+    email: string,
+    authentication?: {
+      password: string
+    },
+    createdAt: NativeDate,
+    updatedAt: NativeDate,
+    __v: number
+  }
 
 export const getAllUsers = async (req: express.Request, res: express.Response) =>{
     try {
