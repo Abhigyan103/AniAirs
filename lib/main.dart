e2e53a33baec2d86fe68e4cca680515b5f290fe8
Astroxphiliauxx <astroxphiliauxx@gmail.com>
2024-01-19 03:35:32 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:AniAirs/src/views/main_page/main_page.dart';
 import 'package:AniAirs/src/views/top_rated.dart/top_rated.dart';
 import 'package:AniAirs/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Future.delayed(Duration(seconds: 3));
+  FlutterNativeSplash.remove();
   runApp(
     const ProviderScope(
       child: MyApp(),
