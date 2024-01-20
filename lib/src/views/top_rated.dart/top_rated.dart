6ff7abd49fc4a93b94ffb07d7c32a3d4dd8adf82
Ananya Singhal <singhalananya67@gmail.com>
2024-01-20 19:42:23 +0530

    Filters

--- lib/src/views/top_rated.dart/top_rated.dart
+++ lib/src/views/top_rated.dart/top_rated.dart
@@ -16,48 +16,72 @@ class TopRated extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var animeListFuture = ref.watch(myTopAnimeFutureProvider);
     List<Anime> animeList = ref.watch(myTopAnimeProvider);
+
     return Scaffold(
-      body: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: [
-          CupertinoSliverRefreshControl(
-            refreshTriggerPullDistance: 150,
-            onRefresh: () async {
-              // return ref.read(myTopAnimeProvider.notifier).setTopAnime();
-            },
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Top Rated',
-                          style: Theme.of(context).textTheme.displayMedium,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              ref.read(showOptionsProvider.notifier).toggle();
-                            },
-                            child: const Text('Filters'))
-                      ]),
-                  ref.watch(showOptionsProvider)
-                      ? Container(
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
+      body: Stack(
+        children: [
+          CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: [
+              CupertinoSliverRefreshControl(
+                refreshTriggerPullDistance: 150,
+                onRefresh: () async {
+                  // return ref.read(myTopAnimeProvider.notifier).setTopAnime();
+                },
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Top Rated',
+                            style: Theme.of(context).textTheme.displayMedium,
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Column(
-                              children: [
-                                DropdownMenu(
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  ref.read(showOptionsProvider.notifier).toggle();
+                                },
+                                child: Row(
+                                  children: [
+                                    Text('Filters'),
+                                    Icon(Icons.arrow_drop_down),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      if (ref.watch(showOptionsProvider)) ...[
+                        Container(
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(top: 10, right: 10),
+                          child: Container(
+                            width: 300,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  spreadRadius: 1.0,
+                                ),
+                              ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  DropdownMenu(
                                     label: const Text('Filter'),
-                                    width:
-                                        MediaQuery.sizeOf(context).width - 32,
+                                    width: 300,
                                     onSelected: (value) {
                                       ref
                                           .read(myTopAnimeProvider.notifier)
@@ -65,8 +89,7 @@ class TopRated extends ConsumerWidget {
                                     },
                                     dropdownMenuEntries: [
                                       ...TopFilter.values.map((e) {
-                                        String item =
-                                            e.toString().split('.')[1];
+                                        String item = e.toString().split('.')[1];
                                         item = item[0].toUpperCase() +
                                             item.substring(1);
                                         return DropdownMenuEntry(
@@ -74,14 +97,14 @@ class TopRated extends ConsumerWidget {
                                       }),
                                       const DropdownMenuEntry(
                                           value: null, label: 'None')
-                                    ]),
-                                const SizedBox(
-                                  height: 16,
-                                ),
-                                DropdownMenu(
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  DropdownMenu(
                                     label: const Text('Type'),
-                                    width:
-                                        MediaQuery.sizeOf(context).width - 32,
+                                    width: 300,
                                     onSelected: (value) {
                                       ref
                                           .read(myTopAnimeProvider.notifier)
@@ -89,8 +112,7 @@ class TopRated extends ConsumerWidget {
                                     },
                                     dropdownMenuEntries: [
                                       ...AnimeType.values.map((e) {
-                                        String item =
-                                            e.toString().split('.')[1];
+                                        String item = e.toString().split('.')[1];
                                         item = item[0].toUpperCase() +
                                             item.substring(1);
                                         return DropdownMenuEntry(
@@ -98,49 +120,53 @@ class TopRated extends ConsumerWidget {
                                       }),
                                       const DropdownMenuEntry(
                                           value: null, label: 'None')
-                                    ]),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                SizedBox(
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  SizedBox(
                                     width: double.infinity,
                                     child: FilledButton(
-                                        onPressed: () {
-                                          ref
-                                              .read(myTopAnimeProvider.notifier)
-                                              .setTopAnime();
-                                        },
-                                        child: Text(
-                                          'Search',
-                                          style:
-                                              TextStyle(color: MyColors.black),
-                                        ))),
-                              ],
+                                      onPressed: () {
+                                        ref
+                                            .read(myTopAnimeProvider.notifier)
+                                            .setTopAnime();
+                                      },
+                                      child: Text(
+                                        'Search',
+                                        style: TextStyle(
+                                            color: MyColors.black),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        )
-                      : const SizedBox()
-                ],
+                        ),
+                      ]
+                    ],
+                  ),
+                ),
               ),
-            ),
+              animeListFuture.when(
+                data: (data) {
+                  return CardGrid(
+                    animeList: animeList,
+                  );
+                },
+                error: (error, stackTrace) {
+                  print('Error : $error');
+                  return const SliverToBoxAdapter();
+                },
+                loading: () {
+                  print('LOADING');
+                  return const SliverToBoxAdapter();
+                },
+              )
+            ],
           ),
-          animeListFuture.when(
-            data: (data) {
-              return CardGrid(
-                animeList: animeList,
-              );
-            },
-            error: (error, stackTrace) {
-              print('Error : $error');
-              return const SliverToBoxAdapter();
-              // return SizedBox();
-            },
-            loading: () {
-              print('LOADING');
-              return const SliverToBoxAdapter();
-              // return SizedBox();
-            },
-          )
         ],
       ),
     );
